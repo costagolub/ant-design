7f8cac31f1de64c39fd748900de698fc59ed7f86
Wei Zhu <yesmeck@gmail.com>
2017-09-07 17:48:16 +0800

    Only add dist warning to dist files

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // This config is for building dist files
 const webpack = require('webpack');
 const getWebpackConfig = require('antd-tools/lib/getWebpackConfig');
+const StringReplacePlugin = require('string-replace-webpack-plugin');
 
 // noParse still leave `require('./locale' + name)` in dist files
 // ignore is better
@@ -19,12 +20,43 @@ function addLocales(webpackConfig) {
   webpackConfig.output.filename = '[name].js';
 }
 
+function addStringReplacePlugin(webpackConfig) {
+  const distPackageWarning = `
+    const ENV = process.env.NODE_ENV;
+    if (ENV !== 'production' &&
+        ENV !== 'test' &&
+        typeof console !== 'undefined' &&
+        console.warn &&
+        typeof window !== 'undefined') {
+      console.warn(
+        'You are using a whole package of antd, ' +
+        'please use https://www.npmjs.com/package/babel-plugin-import to reduce app bundle size.',
+      );
+    }
+  `;
+
+  webpackConfig.module.loaders.push({
+    test: /components\/index\.tsx$/,
+    loader: StringReplacePlugin.replace({
+      replacements: [
+        {
+          pattern: /\/\/ @dist-package-warning/,
+          replacement() {
+            return distPackageWarning;
+          },
+        },
+      ],
+    }),
+  });
+}
+
 module.exports = function (webpackConfig) {
   webpackConfig = getWebpackConfig(webpackConfig, true);
   if (process.env.RUN_ENV === 'PRODUCTION') {
     webpackConfig.forEach((config) => {
       ignoreMomentLocale(config);
       addLocales(config);
+      addStringReplacePlugin(config);
     });
   }
   return webpackConfig;
